936932ecb00228742c8b340496648bfaebe0e7d5
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-15 23:48:43 +0900

    commit2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended:true}));
+const activities = require("./activities.json");
 
 app.get("/", function (req, res) {
     res.sendFile( __dirname + "/index.html" );
@@ -11,6 +12,11 @@ app.post("/autumn", function (req, res){
         res.send("投稿完了");
     });
 });
+
+app.get("/update", function (req, res) {
+    console.log(activities[0].activity);
+    res.send(activities);
+})
 
 app.listen(8080, function()  {
     console.log("Listening on localhost port 8080");
